dc9e600a367da2f58c76d89f6538f13116deea24
walterwhiteop77 <walterwhite2k27@gmail.com>
2026-07-16 00:30:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "26442926"))
+    API_HASH = environ.get("API_HASH", "d091234d2c6e123e6d906d3829eb885b")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "SteveBotz")
+    BOT_SESSION = environ.get("BOT_SESSION", "filmyflixhd")
     DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-    BOT_OWNER = int(environ.get("BOT_OWNER", "6317211079"))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "6725874739"))
     
 class temp(object): 
     lock = {}
